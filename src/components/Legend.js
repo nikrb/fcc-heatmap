4d602a35a90ae6f0d7010793fbf41202e9ab2de1
nikrb <nik_rb@yahoo.com>
2017-07-09 01:21:22 +0100

    move legend to bottom of heatmap

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,15 +1,8 @@
 import React from 'react';
 
 export default (props) => {
-  const legend_style = {
-    position: "absolute",
-    ...props.metrix
-  };
-  const list_wrapper = {
-    padding: "0px"
-  };
   return (
-    <div style={legend_style}>
+    <div>
       Legend
     </div>
   );
